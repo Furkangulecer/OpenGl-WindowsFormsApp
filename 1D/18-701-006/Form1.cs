935f626663b967da4e49e92bdfdf889764d7f8ab
MVC_StokTakip <f-gulecer@hotmail.com>
2021-06-24 12:00:36 +0300

    OpenGl-WindowsFormsApp

---
+++ 1D/18-701-006/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace _18_701_006
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            //ik işlem
+            Gl.glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            Gl.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glColor3f(1.0f, 1.0f, 1.0f);
+            Gl.glBegin(Gl.GL_TRIANGLES);
+               Gl.glVertex3f(0.5f, 1.0f, 0.0f);
+               Gl.glVertex3f(0.0f, 0.0f, 0.0f);
+               Gl.glVertex3f(1.0f, 0.0f, 0.0f);
+            Gl.glEnd();
+        }
+    }
+}
---
+++ Piramit/18-701-006/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tao.OpenGl;
+using Tao.Platform.Windows;
+
+namespace _18_701_006
+{
+    public partial class Form1 : Form
+    {
+        float alfa = 0;
+       
+        public Form1()
+        {
+
+            InitializeComponent();
+            OpenGlControl.InitializeContexts();
+            //ik işlem
+            Gl.glClearColor(0.0f, 0.0f, 0.5f, 0.0f);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            Gl.glLoadIdentity();
+            //Gl.glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+            Gl.glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0);
+
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MyPaint(object sender, PaintEventArgs e)
+        {
+           
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
+            Gl.glShadeModel(Gl.GL_SMOOTH);
+
+            // Gl.glTranslatef(x,y,z);
+            Gl.glPushMatrix();
+
+            Gl.glTranslatef(0.0f, 0.0f, -2.0f); // şekli merkeze taşıdık , z eksenin de iki birim ilerlettik
+            Gl.glRotatef(alfa, 0.0f, 1.0f, 0.0f);
+            Gl.glRotatef(alfa, 1.0f, 0.0f, 0.0f);
+
+           Gl.glTranslatef(0.0f, 0.0f, 2.0f); // şekli z ekesninde 2 birim götürmüştük aynı miktarda geri getirdik
+            Gl.glTranslatef(0.5f, 0.5f, 0); //geri
+            Gl.glRotatef(alfa, 0, 0, 1);
+            Gl.glTranslatef(-0.5f, -0.5f, 0); //merkez çakıştırma
+            Gl.glBegin(Gl.GL_TRIANGLES);
+               Gl.glColor3f(1.0f, 0.0f, 0.0f); //kırmızı
+               Gl.glVertex3f(0.5f, 1.0f, 0.0f);
+
+               Gl.glColor3f(1.0f, 1.0f, 0.0f);  //sarı
+               Gl.glVertex3f(0.0f, 0.0f, 0.0f);
+
+               Gl.glColor3f(0.0f, 0.0f, 0.3f);  
+               Gl.glVertex3f(1.0f, 0.0f, 0.0f);
+
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+            Gl.glVertex3f(1.0f, 1.0f, -1.0f);
+            Gl.glVertex3f(0.0f, 3.0f, -2.0f);
+
+            // Pramit Sol Parça
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(-1.0f, 1.0f, -1.0f);
+            Gl.glVertex3f(-1.0f, 1.0f, -3.0f);
+            Gl.glVertex3f(0.0f, 3.0f, -2.0f);
+
+            // Pramit Arka Parça
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glVertex3f(-1.0f, 1.0f, -3.0f);
+            Gl.glVertex3f(1.0f, 1.0f, -3.0f);
+            Gl.glVertex3f(0.0f, 3.0f, -2.0f);
+
+            // Pramit Sağ Parça
+            Gl.glColor3f(0.0f, 0.0f, 1.0f);
+            Gl.glVertex3f(1.0f, 1.0f, -3.0f);
+            Gl.glVertex3f(1.0f, 1.0f, -1.0f);
+            Gl.glVertex3f(0.0f, 3.0f, -2.0f);
+            
+            Gl.glEnd();
+            Gl.glPopMatrix();
+
+
+            
+        }
+
+        private void buttonDondur_Click(object sender, EventArgs e)
+        {
+            alfa = (alfa + 10) % 360;
+            OpenGlControl.Refresh();
+             
+        }
+
+        private void MyKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.F)
+                alfa = (alfa + 5) % 360;
+            if (e.KeyCode == Keys.G)
+                alfa = (alfa - 5) % 360;
+            OpenGlControl.Refresh();
+
+
+        }
+
+        private void OpenGlControl_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
